6134843345de0876d95b4448286a141a1aa1c1d2
Chadwell Clark <wcclark23@comcast.net>
2021-06-28 12:04:04 -0500

    Finished Get all My Posts

--- TabloidMVC/Repositories/IPostRepository.cs
+++ TabloidMVC/Repositories/IPostRepository.cs
@@ -7,7 +7,9 @@ namespace TabloidMVC.Repositories
     {
         void Add(Post post);
         List<Post> GetAllPublishedPosts();
+        List<Post> GetAllPublishedPostsByUser(int id);
         Post GetPublishedPostById(int id);
         Post GetUserPostById(int id, int userProfileId);
+        
     }
 }
