b0f6a02a2bc6a565c74f035ae84af10992b93633
Preston Shotts <shotts.preston@gmail.com>
2021-06-29 15:40:02 -0500

    view comments

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+
+        public List<Comment> GetAllCommentsByPostId(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, PostId, UserProfileId, Subject, content, CreateDateTime
+                                        FROM Comment
+                                        WHERE PostId = @id
+                                        ";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        comments.Add(new Comment()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileID = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        });
+                    }
+
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+        public void AddComment(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ICommentRepository
+    {
+        List<Comment> GetAllCommentsByPostId(int id);
+        public void AddComment(Comment comment);
+    }
+}
