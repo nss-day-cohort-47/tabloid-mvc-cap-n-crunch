53fa0f9f636f11571aa9cefbc900df76a2bd51e3
Joel Gage <jrossgage@gmail.com>
2021-06-29 09:15:23 -0500

    Category list is now viewable

---
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly IPostRepository _postRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(IPostRepository postRepository, ICategoryRepository categoryRepository)
+        {
+            _postRepository = postRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _categoryRepository.GetAll();
+            return View(categories);
+        }
+
+        //        public IActionResult Details(int id)
+        //        {
+        //            var post = _postRepository.GetPublishedPostById(id);
+        //            if (post == null)
+        //            {
+        //                int userId = GetCurrentUserProfileId();
+        //                post = _postRepository.GetUserPostById(id, userId);
+        //                if (post == null)
+        //                {
+        //                    return NotFound();
+        //                }
+        //            }
+        //            return View(post);
+        //        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            try
+            {
+
+                _categoryRepository.AddCategory(category);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        //        public IActionResult Delete(int id)
+        //        {
+        //            Post post = _postRepository.GetPublishedPostById(id);
+        //            if (post.UserProfileId == GetCurrentUserProfileId())
+        //            {
+        //                return View(post);
+        //            }
+        //            else
+        //            {
+        //                return Unauthorized();
+        //            }
+        //        }
+
+        //        [HttpPost]
+        //        [ValidateAntiForgeryToken]
+        //        public IActionResult Delete(int id, Post post)
+        //        {
+        //            try
+        //            {
+        //                _postRepository.DeletePost(id);
+        //                return RedirectToAction("Index");
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                return View(post);
+        //            }
+        //        }
+
+        //        public IActionResult Edit(int id)
+        //        {
+        //            Post post = _postRepository.GetPublishedPostById(id);
+        //            int userId = GetCurrentUserProfileId();
+        //            if (post == null || userId != post.UserProfileId)
+        //            {
+        //                return NotFound();
+        //            }
+        //            return View(post);
+        //        }
+        //        // POST: DogsController/Edit/5
+        //        [HttpPost]
+        //        [ValidateAntiForgeryToken]
+        //        public IActionResult Edit(int id, Post post)
+        //        {
+        //            //try
+        //            //{
+        //            post.UserProfileId = GetCurrentUserProfileId();
+        //            _postRepository.UpdatePost(post);
+        //            return RedirectToAction("Index");
+        //            //}
+        //            //catch (Exception ex)
+        //            //{
+        //            //    return View(post);
+        //            //}
+        //        }
+
+        //        private int GetCurrentUserProfileId()
+        //        {
+        //            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        //            return int.Parse(id);
+        //        }
+        //    }
+        //}
+    }
+}
--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using TabloidMVC.Models;
 
@@ -14,7 +15,7 @@ namespace TabloidMVC.Repositories
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT id, name FROM Category";
+                    cmd.CommandText = "SELECT id, name FROM Category order by name";
                     var reader = cmd.ExecuteReader();
 
                     var categories = new List<Category>();
@@ -31,6 +32,29 @@ namespace TabloidMVC.Repositories
                     reader.Close();
 
                     return categories;
+                }
+            }
+        }
+
+        public void AddCategory(Category category)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Category ([Name])
+                    OUTPUT INSERTED.ID
+                    VALUES (@name);
+                ";
+
+                    cmd.Parameters.AddWithValue("@name", category.Name);
+                
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    category.Id = id;
                 }
             }
         }
